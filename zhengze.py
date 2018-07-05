5fbd6263f547659bcb7da9147f5ace73598cf75e
barricade <barricadeiscool@outlook.com>
2018-07-05 19:16:32 +0800

    something new

--- zhengze.py
+++ zhengze.py
@@ -1,4 +1,5 @@
 import re
+import requests
 #.的使用 站位符，不包括换行符
 #secrer_code2 = 'Quotes to Scrape'
 #secret_code = 'hadk.fali-fxxIxxfasdjifja134xxlovexx2345sdfxxyouxx8dfse'
@@ -24,7 +25,7 @@ import re
 #print(b,end-start)
 
 #re.match(patten,string,flags=0)
-content = 'Hello 123 4567 World_This is a Regex Demo'
+#content = 'Hello 123 4567 World_This is a Regex Demo'
 
 #常规匹配
 #result = re.match('^Hello\s\d\d\d\s\d\d\d\d\s.*Demo$',content)
@@ -45,7 +46,40 @@ content = 'Hello 123 4567 World_This is a Regex Demo'
 #7
 
 #非贪婪匹配
-#result = re.match('^He.*?(\d+.*)\d.*Demo$',content)
+#result = re.match('^He.*?(\d+.*)W.*Demo$',content)
 #print(result.group(1))
 #123 4567
 
+#匹配模式
+#result = re.match("^Hell.*?(\d+).*?Demo$",content,re.s)
+#.无法匹配换行符
+
+#转义
+#加入\$ 匹配$符号
+
+#re.search扫描整个字符串并返回第一个成功的匹配
+#content = 'Extra strings Hello 1234567 World_This is a Regex Demo Extra strings'
+#result = re.match('^He.*?(\d+.*)\d.*Demo$',content)
+#none
+#result = re.search('^Ex.*?(\d+.*)W.*Demo$',content)
+#print(result.group(1))
+
+#re.findall搜索字符串，以列表形式返回全部能匹配的子串
+
+#re.sub 替换字符串中每一个匹配的子串后返回替换后的字符串
+#content = re.sub('\d+','Replacement',content)
+#print(content)
+#Extra strings Hello Replacement World_This is a Regex Demo Extra strings
+#content = re.sub('(\d+)',r'\1 8910',content)
+#print(content)
+
+#爬去书名实践
+content = requests.get('http://book.douban.com/').text
+patten = re.compile('"book-info".*?href="(.*?)".*?blank">(.*?)</a>.*?author">(.*?)</div>',re.S)
+results = re.findall(patten,content)
+for result in results:
+    url,name,author = result
+    url = re.sub('\s','',url)
+    name = re.sub('\s','',name)
+    author = re.sub('\s','',author)
+    print(url,name,author)
